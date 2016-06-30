0288df2567a9c242581f06c25614fd037f83d6ae
Tyler Barton <tbarton33@outlook.com>
2016-06-29 23:42:44 -0500

    Some changes were made, but I'm unsure if I will keep them

--- WebScrapper/WebScrapper/Form1.Designer.cs
+++ WebScrapper/WebScrapper/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WebScrapper
 {
-    partial class Form1
+    partial class webscrapperForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // webscrapperForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(707, 317);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "webscrapperForm";
+            this.Text = "Web Scrapping Tester";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.DirectoryServices.DirectoryEntry directoryEntry1;
     }
 }
 
--- WebScrapper/WebScrapper/Form1.cs
+++ WebScrapper/WebScrapper/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WebScrapper
 {
-    public partial class Form1 : Form
+    public partial class webscrapperForm : Form
     {
-        public Form1()
+        public webscrapperForm()
         {
             InitializeComponent();
         }
