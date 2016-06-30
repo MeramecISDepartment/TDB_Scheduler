57502d183a44a3e51b8d742cfac2e0c3e7460d5d
Tyler Barton <tbarton33@outlook.com>
2016-06-30 00:04:32 -0500

    Added old projects to the folder as reference; FinalProject_Step1 was not the right project, but Webpage_Reader is

---
+++ TDB_Scheduler/Webpage_Reader/Webpage_Reader/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Webpage_Reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.url_Textbox = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.webPageReaderTabs = new System.Windows.Forms.TabControl();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.tabPage2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.webPageReaderTabs.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button6);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(730, 300);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Text To  Audio";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button7);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Controls.Add(this.url_Textbox);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(730, 300);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Get Web Text";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(525, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Get Source Code";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // url_Textbox
+            // 
+            this.url_Textbox.Location = new System.Drawing.Point(94, 18);
+            this.url_Textbox.Name = "url_Textbox";
+            this.url_Textbox.Size = new System.Drawing.Size(426, 20);
+            this.url_Textbox.TabIndex = 8;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(94, 69);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(477, 207);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Enter URL here:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Source Code:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(599, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Edit Source Code";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(599, 161);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Save File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // webPageReaderTabs
+            // 
+            this.webPageReaderTabs.Controls.Add(this.tabPage1);
+            this.webPageReaderTabs.Controls.Add(this.tabPage2);
+            this.webPageReaderTabs.Controls.Add(this.tabPage3);
+            this.webPageReaderTabs.Location = new System.Drawing.Point(12, 12);
+            this.webPageReaderTabs.Name = "webPageReaderTabs";
+            this.webPageReaderTabs.SelectedIndex = 0;
+            this.webPageReaderTabs.Size = new System.Drawing.Size(738, 326);
+            this.webPageReaderTabs.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Get Text File:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(560, 65);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Read File Aloud";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(560, 36);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 23);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Convert File To MP3";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(107, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(299, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(320, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Your Audio File Is Ready.";
+            this.label4.Visible = false;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(343, 207);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "OK";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.panel1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(730, 300);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Listen To Audio";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(16, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(704, 282);
+            this.panel1.TabIndex = 0;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(599, 117);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(113, 23);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Create File";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Visible = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 350);
+            this.Controls.Add(this.webPageReaderTabs);
+            this.Name = "Form1";
+            this.Text = "Web Page Reader";
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.webPageReaderTabs.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox url_Textbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl webPageReaderTabs;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button7;
+
+    }
+}
+
---
+++ TDB_Scheduler/Webpage_Reader/Webpage_Reader/Form1.cs
@@ -0,0 +1,192 @@
+﻿/*
+I started this project today: 12/14/2015
+
+ 
+I reference this video tutorial: https://www.youtube.com/watch?v=NyT33tOkxYQ 
+(C# Tutorial 91: How to get Source Code from a Website with C#)
+published by ProgrammingKnowledge.
+
+All of the following code is by him so far.
+
+---------------------------------------------------------------------------------------------------------------
+Get the sourcecode
+in the window
+edit it
+save it to a file
+in another tab 
+open the file
+convert the file to an audio file or read it out
+--------------------------------------------------
+ My final project is designed to take a website, scrape the text from it, and convert that text into an 
+ audio file.  How it works is that the user gives it a URL.  The program verifies that the URL exists 
+ and then gets the source code from that website.  The program then scrapes out only the text from that
+ website, disregarding the tags.  It then converts the text to audio and stores it as an mp3 so that the 
+ user can listen to the web page in the future.
+
+The inputs are a URL entered by the user, the source code, the text of the web page and the audio of that text.
+These inputs come from the user, who entered the URL or copied and pasted it, the web page, it's source code, 
+the  text from the source code and the audio from that text. 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+// I referenced http://www.dotnetperls.com/streamwriter for how to include StreamWriter
+using System.IO;
+
+namespace Webpage_Reader
+{
+    public partial class Form1 : Form
+    {
+        /*public string textFileName = "";*/
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = url_Textbox.Text;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            richTextBox1.Text = sr.ReadToEnd();
+            sr.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            button2.Visible = true;
+            /*button7.Visible = true;*/
+            button3.Visible = true;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            /*I referenced this Code Project page
+             http://www.codeproject.com/Questions/213245/how-to-enable-disable-textbox
+             to enable the richTextBox1 text field for editing.*/
+            richTextBox1.Enabled = true;
+        }
+
+        /*public Form2 nameFileForm;*/
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*string newTextFileName = nameFileForm.textFileName;*/
+            /*I referenced https://www.youtube.com/watch?v=hs74fKPJpFw for how to write to a file.*/
+            StreamWriter File = new StreamWriter("newTextFile.txt");
+            File.Write(richTextBox1.Text);
+            /*string webText = richTextBox1.Text;
+            System.IO.File.WriteAllLines(@"C:\Users\Tyler Barton\Desktop\", webText);*/
+            /*I referenced here 
+             * https://msdn.microsoft.com/library/ms752080(v=vs.100).aspx
+             * to create a message box.
+             */
+            
+            MessageBox.Show("Your text file was saved.");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // I referenced this page on Stack Overflow:
+            // http://stackoverflow.com/questions/5718183/how-to-open-the-second-form
+            // to open a seperate form window.
+            //
+            // Create a new instance of the Form2 class
+            Form2 nameFileForm = new Form2();
+
+            // Show the settings form
+            nameFileForm.Show();
+
+            /*button3.Visible = true;*/
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string locationOfFile = textBox2.Text;
+
+            /*
+            SaveFileDialog my_Sfd = new SaveFileDialog();
+            my_Sfd.Filter = "All files (*.*)|*.*|wav files (*.wav)|*.wav";
+            my_Sfd.Title = "Save to a wav file.";
+            my_Sfd.FilterIndex = 2;
+            my_Sfd.RestoreDirectory = true;
+            if ((my_Sfd.ShowDialog() == DialogResult.OK)&&(TxtArea_Chk.Checked == true))
+                {
+                spFileStream.Open(my_Sfd.FileName, spFileMode, false);
+                my_Voice.AudioOutputStream = spFileStream;
+                my_Voice.Speak(TextArea_User.Text, my_Spflag);
+                my_Voice.WaitUntilDone(-1);
+                spFileStream.Close();
+                }
+            else if((my_Sfd.ShowDialog() == 
+		DialogResult.OK)&&(TxtFilePath_Chk.Checked == true))
+                {
+                spFileStream.Open(my_Sfd.FileName, spFileMode, false);
+                my_Voice.AudioOutputStream = spFileStream;
+                my_Voice.Speak(my_fDlg.FileName, SpeechVoiceSpeakFlags.SVSFIsFilename);
+                my_Voice.WaitUntilDone(-1);
+                spFileStream.Close();
+                }
+             */
+        }
+        /*
+         * Referencing Prof. Oberst's TextFile.cs file in the ConsoleReadInvitation Solution.
+         * 
+        class TextFile
+        {
+            public string InFile { get; set; }
+
+            public List<string> LinesRead { get; set; }
+
+            public TextFile(string inFile = "") 
+            {
+                if (inFile.Length > 0)
+                {
+                    this.InFile = inFile;
+                    ReadFile();
+                }
+            }
+
+	        // Read in a file line-by-line, and store it all in a List.
+            private void ReadFile() 
+            {
+	            List<string> list = new List<string>();
+
+	            using (StreamReader reader = new StreamReader(this.InFile))
+	            {
+	                string line;
+	                while ((line = reader.ReadLine()) != null)
+		                list.Add(line); // Add to list.
+	            }
+
+                this.LinesRead = list;
+            }
+         */
+
+    }
+}
